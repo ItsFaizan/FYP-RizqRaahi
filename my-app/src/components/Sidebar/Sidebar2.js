a4ec01480cb9020ca774c6410287c65f5bc8599d
Ghazanfarijaz <i200870@nu.edu.pk>
2024-03-26 09:56:50 +0500

    Sidebar Responsive Complete

--- my-app/src/components/Sidebar/Sidebar2.js
+++ my-app/src/components/Sidebar/Sidebar2.js
@@ -14,8 +14,14 @@ library.add(fas);
 
 const Sidebar2 = () => {
 
+ 
+  const [isMobileView, setIsMobileView] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const { t, i18n } = useTranslation();
   const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+
+  
 
 const handleLanguageSwitch = () => {
   if (currentLanguage === "en") {
@@ -29,7 +35,7 @@ const handleLanguageSwitch = () => {
   }
 };
 
-  const [isExpanded, setIsExpanded] = useState(false);
+  
   const navigate = useNavigate();
   const locationdata = useLocation();
   const data = locationdata.state;
@@ -61,6 +67,29 @@ const handleLanguageSwitch = () => {
   }, [token])
 );
 
+///=========================
+
+useEffect(() => {
+  const handleResize = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobileView(true);
+      setIsExpanded(false); // Close sidebar in mobile view by default
+    } else {
+      setIsMobileView(false);
+    }
+  };
+
+  handleResize(); // Call the function to check the initial width
+  window.addEventListener('resize', handleResize);
+
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
+
+//==========================
+
   const handleNavigateTo = (path) => {
     // navigate('/chats', { state: { option: option, token: data.token } });
     navigate(path, { state: { option: "Restaurant", token } });
@@ -69,13 +98,26 @@ const handleLanguageSwitch = () => {
   return (
     <div className='font-[Inter]'>
     <aside className="fixed h-full">
-      <nav
-        className={`${
-          isExpanded
-            ? 'w-64 transition-width duration-300 ease-in-out'
-            : 'w-16 transition-width duration-300 ease-in-out'
-        } h-full flex flex-col bg-white border-r shadow-2xl relative`}
-      >
+    <nav
+          className={`
+          
+          
+          ${
+            isMobileView
+              ?
+
+            isExpanded
+              ? 'w-64 transition-width duration-300 ease-in-out   h-full flex flex-col bg-white border-r shadow-2xl relative'
+              : 'w-16 transition-width duration-300 ease-in-out   '
+              
+
+              : isExpanded
+              ? 'w-64 transition-width duration-300 ease-in-out   h-full flex flex-col bg-white border-r shadow-2xl relative'
+              : 'w-16 transition-width duration-300 ease-in-out   h-full flex flex-col bg-white border-r shadow-2xl relative'
+          } 
+          
+          `}
+        >
         <div className="p-4 pb-2 flex justify-between items-center">
           {isExpanded && (
             <img
@@ -101,6 +143,22 @@ const handleLanguageSwitch = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
               </svg>
             ) : (
+
+              <>
+
+              { isMobileView ? (
+
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="h-5 w-5 text-gray-600"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+
+              ) : (
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -110,11 +168,20 @@ const handleLanguageSwitch = () => {
               >
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 15l7-7 7 7" />
               </svg>
+
+              ) }
+
+</>
             )}
           </button>
         </div>
 
+{isMobileView ? (
+
+<>
+{isExpanded ? (
         <ul className="flex-1 px-3">
+         
           <li
             className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
             onClick={() => handleNavigateTo('/donationAnnouncement')}
@@ -259,7 +326,164 @@ const handleLanguageSwitch = () => {
           </li>
 
           {/* Add more manual entries for other buttons as needed */}
+
+          
+          
         </ul>
+ ) : (<></>) }  
+
+  </>
+
+) : (
+
+  <ul className="flex-1 px-3">
+  <li
+    className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+    onClick={() => handleNavigateTo('/donationAnnouncement')}
+  >
+    <span className="group-hover:bg-indigo-50 flex items-center w-full">
+      <FontAwesomeIcon
+        icon="home"
+        size="lg"
+        className="group-hover:text-indigo-800 mr-2 text-green-500"
+      />
+      <span
+        className={`transition-all group-hover:bg-indigo-50 mr-2 ${
+          isExpanded ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+        }`}
+      >
+        {t('drawerHome')}
+      </span>
+    </span>
+  </li>
+
+  <li
+  
+  onClick={() => handleNavigateTo('/chats')}
+    className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+    
+  >
+    <span  className="group-hover:bg-indigo-50 flex items-center w-full">
+      <FontAwesomeIcon
+        icon="comment"
+        size="lg"
+        className="group-hover:text-indigo-800 mr-2 text-green-500"
+      />
+      <span
+        className={`transition-all group-hover:bg-indigo-50 mr-2 ${
+          isExpanded ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+        }`}
+      >
+        {t('chatword')}
+      </span>
+    </span>
+  </li>
+
+  <li
+    className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+    onClick={() => handleNavigateTo('/deliverytracker')}
+  >
+    <span  className="group-hover:bg-indigo-50 flex items-center w-full">
+      <FontAwesomeIcon
+        icon="hand-holding-heart"
+        size="lg"
+        className="group-hover:text-indigo-800 mr-2 text-green-500"
+      />
+      <span
+        className={`transition-all group-hover:bg-indigo-50 mr-2 ${
+          isExpanded ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+        }`}
+      >
+        {t('donationsword')}
+      </span>
+    </span>
+  </li>
+  
+  <li
+    className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+    onClick={() => handleNavigateTo('/crisisdonationstatus')}
+  >
+    <span  className="group-hover:bg-indigo-50 flex items-center w-full ">
+      <FontAwesomeIcon
+        icon="fa-solid fa-handshake"
+        size="lg"
+        className="group-hover:text-indigo-800 mr-2 text-green-500"
+      />
+      <span
+        className={`transition-all group-hover:bg-indigo-50 mr-2 ${
+          isExpanded ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+        }`}
+      >
+        {t('crisisdonotext')}
+      </span>
+    </span>
+  </li>
+
+  <li
+    className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+    onClick={() => handleNavigateTo('/')}
+  >
+    <span  className="group-hover:bg-indigo-50 flex items-center w-full">
+      <FontAwesomeIcon
+        icon="power-off"
+        size="lg"
+        className="group-hover:text-indigo-800 mr-2 text-green-500"
+      />
+      <span
+        className={`transition-all group-hover:bg-indigo-50 mr-2 ${
+          isExpanded ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'
+        }`}
+      >
+        {t('drawerSignout')}
+      </span>
+    </span>
+  </li>
+
+  <li
+    className="relative flex items-center py-2 px-3 my-4 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600"
+    onClick={() => 
+    {
+      
+      if (isExpanded)
+      {
+        handleLanguageSwitch();
+      }
+      else
+      {
+        setIsExpanded(true)
+      }
+    }
+
+    }
+  >
+    <span  className="group-hover:bg-indigo-50 flex items-center w-full">
+      {isExpanded ? (
+        <img
+          src={currentLanguage === 'en' ? languageen : languageur}
+          alt=""
+          className="h-8 w-24 mr-2"
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon="language"
+          size="xl"
+          className="group-hover:text-indigo-800 mr-2 text-green-500"
+        />
+      )}
+      <span
+        className={`transition-all group-hover:bg-indigo-50 mr-2 ${
+          isExpanded ? 'opacity-0' : 'opacity-0 group-hover:opacity-100'
+        }`}
+      >
+        {t('switchlanguagetext')}
+      </span>
+    </span>
+  </li>
+
+  {/* Add more manual entries for other buttons as needed */}
+</ul>
+
+) }
 
         {isExpanded && (
           <div className="border-t flex p-3">
